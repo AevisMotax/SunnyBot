d2af1e4817c453a0c3373f2de6e834c19eda5afe
Julian Aloise <julian.aloise@mail.concordia.ca>
2025-02-01 18:54:27 -0500

    added Queries

--- database.py
+++ database.py
@@ -7,26 +7,38 @@ uri = "mongodb+srv://conuhacksunlife:conuhacks2025@financesunlife.pmyry.mongodb.
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
 
+#access database Finance and collection Users
+mydb = client["Finance"]
+usercollection = mydb["Users"]   
+
+#user inputs email and password
+myemail = input("Email: ")
+mypassword = input ("Password: ")
+userinfo = usercollection.find_one({"email": myemail,"password": mypassword})
 
 
-#for x in mycol.find():
-#    print(x)
-
-def getUser():
-    #access database Finance and collection Users
-    mydb = client["Finance"]
-    usercollection = mydb["Users"]   
-
-    #user inputs email and password
-    myemail = input("Email: ")
-    mypassword = input ("Password: ")
-
+def QueryUserInfo(myemail,mypassword):
     #checks if username and password is there
-    userinfo = usercollection.find_one({"email": myemail,"password": mypassword})
+    #userinfo = usercollection.find_one({"email": myemail,"password": mypassword})
     if userinfo == None: 
-        return print("Email and/or passowrd is incorrect.")
+       return print("Email and/or passowrd is incorrect.")
     else:
-        print(userinfo)
+       print(userinfo)
 
+def QueryID(myemail,mypassword):
+    #userinfo = usercollection.find_one({"email": myemail})
+    if userinfo:
+        return print(userinfo['_id'])
+    else: 
+        return None
+    
+def QueryBalance(myemail,mypassword):
+    if userinfo:
+        return print(userinfo['Balance'])
+    else: 
+        return None
 
-getUser()
+#QueryUserInfo(myemail,mypassword)
+#QueryID(myemail,mypassword)
+QueryBalance(myemail,mypassword)
+
