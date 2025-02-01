7c064162e9b6edde94b1d1422da97a041cdb72d1
Aldrex1 <alexandruilie1301@gmail.com>
2025-02-01 18:05:37 -0500

    modular api

--- backend/llm.py
+++ backend/llm.py
@@ -6,14 +6,17 @@ import database
 user = database.getUser()[0]
 app = FastAPI()
 
+async def base_chat(prompt):
+    message = {'role': 'user', 'content': f'{prompt}: {user}'}
+    final_message = ""
+    async for part in await AsyncClient().chat(model='deepseek-r1:8b', messages=[message], stream=True):
+        print(part['message']['content'], end='', flush=True)
+        final_message += part['message']['content']
+    return {'message': final_message}
+
 @app.get('/chat')
 async def chat():
-  message = {'role': 'user', 'content': f'Please write a financial summary of the following person: {user}'}
-  final_message = ""
-  async for part in await AsyncClient().chat(model='deepseek-r1:8b', messages=[message], stream=True):
-      print(part['message']['content'], end='', flush=True)
-      final_message += part['message']['content']
-  return {'message': final_message}
+    return await base_chat("Please write a financial summary of the following person")
 
 #asyncio.run(chat())
 
