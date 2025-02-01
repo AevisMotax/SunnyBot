04e5e40264a65a680abdc357b6c2c50d63fe9b1b
Julian Aloise <julian.aloise@mail.concordia.ca>
2025-02-01 16:48:10 -0500

    check return values based on email

--- database.py
+++ database.py
@@ -6,21 +6,24 @@ uri = "mongodb+srv://conuhacksunlife:conuhacks2025@financesunlife.pmyry.mongodb.
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
 
-# Send a ping to confirm a successful connection
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
 
-mydb = client["Finance"]
-mycol = mydb["Users"]
 
 #for x in mycol.find():
 #    print(x)
 
-myquery = {"FirstName": "Julian"}
+def getUser():
+    #access database Finance and collection Users
+    mydb = client["Finance"]
+    mycol = mydb["Users"]   
 
-mydoc = mycol.find(myquery)
-for x in mydoc:
-    print(x)
+    #user inputs email and password
+    myemail = input("Email: ")
+    password = input("Password: ")
+
+    #checks if username and password is there
+    userinfo = mycol.find_one({"email": myemail})
+    print(myemail)
+    print (userinfo)
+
+
+getUser()
