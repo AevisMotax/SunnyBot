c053d948eba5941c516e0628f0a821aca50f683d
AevisMotax <robert.chen@mail.concordia.ca>
2025-02-01 16:44:02 -0500

    starting balance Card

---
+++ webclient/src/chartJsSetup.js
@@ -0,0 +1,16 @@
+import { Line } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+
+// Register required components for Chart.js
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
+
+export { Line };
