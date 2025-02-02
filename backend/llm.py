1a0da6a8df600e839861ab2bbafd379dae3fbcc2
Aldrex1 <alexandruilie1301@gmail.com>
2025-02-02 09:00:56 -0500

    llm financial advice api pop up

--- backend/llm.py
+++ backend/llm.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI, APIRouter
 from ollama import AsyncClient
 import database
 import re
+from pydantic import BaseModel
 
 user = database.QueryUserInfo(database.myemail, database.mypassword)
 app2_router = APIRouter()
@@ -17,9 +18,21 @@ async def base_chat(prompt):
     return {'message': final_message}
 
 @app2_router.get('/balance-chat')
-async def chat():
+async def balance_chat():
     return await base_chat("Please give advice to this person consider only their balance and any balance history: ")
 
 #asyncio.run(chat())
+
+#@app2_router.post('/balance')
+#async def pop_chat():
+#    return None
+
+class MessageInput(BaseModel):
+    message: str
+@app2_router.post("/financial-chat")
+async def pop_chat(input_data: MessageInput):
+    message = input_data.message
+    prompt = f"A user on our Sun Life financial platform wants to ask you a financial related question. Here is the question: {message}. Now please give a personalized answer considering the following information on the person"
+    return await base_chat(prompt)
 
 
