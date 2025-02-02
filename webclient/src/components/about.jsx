63de0421097af1d692cb7b33cd675811eee630f3
AevisMotax <robert.chen@mail.concordia.ca>
2025-02-02 07:57:58 -0500

    adding about

--- webclient/src/components/about.jsx
+++ webclient/src/components/about.jsx
@@ -1,12 +1,22 @@
 
-
+import { Typography } from "@mui/material";
 
 export default function About() {
 
     return (
         <>
-        
-        
+            <Typography>
+                Who Sun Life Financial are
+            </Typography>
+
+            <section>
+                Sun Life understands that financial management can be complex, personal and time-consuming.
+                 When we help clients with their financial management needs, we enable them to gain the 
+                 freedom to live their lives their way, from now through retirement. 
+                 And we’re working hard to support their wellness and health management goals, 
+                 so they can enjoy what matters most to them – whether that’s running a marathon, 
+                 climbing Mount Kilimanjaro or helping their grandchildren learn to ride a bike.
+            </section>
         </>
     );
 }
