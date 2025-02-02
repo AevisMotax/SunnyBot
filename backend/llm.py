c408bba4b3fb9c659424855217a1ad81cff8d159
Aldrex1 <alexandruilie1301@gmail.com>
2025-02-02 08:09:16 -0500

    minor change llm

--- backend/llm.py
+++ backend/llm.py
@@ -2,6 +2,7 @@ import asyncio
 from fastapi import FastAPI, APIRouter
 from ollama import AsyncClient
 import database
+import re
 
 user = database.QueryUserInfo(database.myemail, database.mypassword)
 app2_router = APIRouter()
@@ -12,6 +13,7 @@ async def base_chat(prompt):
     async for part in await AsyncClient().chat(model='deepseek-r1:8b', messages=[message], stream=True):
         print(part['message']['content'], end='', flush=True)
         final_message += part['message']['content']
+    final_message = re.sub(r'<think>.*?</think>', '', final_message, flags=re.DOTALL)
     return {'message': final_message}
 
 @app2_router.get('/balance-chat')
